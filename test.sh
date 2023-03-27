d242547dda94a7a3f597c336250cfffbd89d1272
Mark Meysenburg <mark.meysenburg@doane.edu>
2023-03-27 08:33:31 -0500

    Doh - MovieDataTest, not MovieData

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 javac -classpath .:./jars/*:target/dependency/* -d . $(find . -type f -name '*.java')
-java -classpath .:./jars/*:target/dependency/* MovieData
+java -classpath .:./jars/*:target/dependency/* MovieDataTest
